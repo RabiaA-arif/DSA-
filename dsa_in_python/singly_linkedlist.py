19cf1465bb244356dfced477dd80357ea56a0374
RabiaA-arif <rabiaarif694@gmail.com>
2026-01-07 16:51:26 +0500

    Add basic singly linked list implementation
    Introduces Node and singly_linkedlist classes with constructors to initialize node data and the list head.

---
+++ dsa_in_python/singly_linkedlist.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,info,next=None):
+        self.data=info
+        self.next=next
+        
+        
+class singly_linkedlist:
+    def __init__(self,head=None):
+        self.head=head
