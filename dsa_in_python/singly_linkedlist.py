c7e34e14b58995ce836c8c30c47c631da3d18468
RabiaA-arif <rabiaarif694@gmail.com>
2026-01-08 19:41:16 +0500

    Add insert and delete methods to singly linked list
    Added insert_node_at_mid to insert a node after a given value and deletell to remove a node by value. Updated usage examples to demonstrate the new methods.

--- dsa_in_python/singly_linkedlist.py
+++ dsa_in_python/singly_linkedlist.py
@@ -26,6 +26,28 @@ class singly_linkedlist:
         self.head=temp
     
     
+    
+    def insert_node_at_mid(self,value,x):
+        temp=Node(value)
+        t1=self.head
+        
+        while(t1.next!=None):
+            if(t1.data==x):
+                temp.next=t1.next
+                t1.next=temp
+            t1=t1.next
+        
+    def deletell(self,value):
+        temp1=self.head
+        prev=temp1
+        while(temp1.next!=None):
+            if(temp1.data==value):
+                prev.next=temp1.next
+                break
+            else:
+                prev=temp1
+                temp1=temp1.next
+    
     def printll(self):
         t1=self.head
         while(t1.next !=None):
@@ -48,4 +70,7 @@ obj.insert_node_at_begning(30)
 obj.insert_node_at_begning(40)
 obj.insert_node_at_begning(50)
 
+obj.insert_node_at_mid(35,20)
+obj.insert_node_at_mid(11,10)
+obj.deletell(30)
 obj.printll()
